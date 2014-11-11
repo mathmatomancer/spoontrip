f58501dfbcb63313facd68bab665f202d3259352
Joseph Chen <joseph@urbanspoon.com>
2014-11-11 11:57:09 -0800

    First cut of adding infowindow popup for map markers

--- app/assets/javascripts/maps/maps.js
+++ app/assets/javascripts/maps/maps.js
@@ -5,7 +5,8 @@ var MapSettings = {
     starting_coordinates: null,
     path: [],
     argument_string: '',
-    markers: []
+    markers: [],
+    infoWindow: undefined
 };
 
 function mapCenter() {
@@ -45,18 +46,50 @@ function mapStart() {
     $('#fetch-data').hide();
 }
 
+function showInfoWindow(marker) {
+  if (typeof MapSettings.infoWindow != 'undefined') {
+    MapSettings.infoWindow.close();
+  }
+  MapSettings.infoWindow = new google.maps.InfoWindow({content: marker.content});
+  MapSettings.infoWindow.open(MapSettings.map, marker);
+}
+
+function getInfoWindowHtml(data) {
+  return '' +
+    '<div class="info-window">' +
+      '<a href="'+data.urbanspoon_url+'">'+data.title+'</a>' +
+      ((data.total_votes >= 10) ? '<br />'+data.votes_percent+'% like it' : '') +
+      '<br />' +
+      ((data.price) ? data.price + '/4 ' : '') +
+      ((data.cuisines.length > 0) ? data.cuisines[0].title : '') +
+      '<br />' +
+      data.phone +
+      '<br />' +
+      data.address.street + ', ' + data.address.city + ', ' + data.address.state +
+    '</div>';
+}
+
+function setMarker(data) {
+  var marker = new google.maps.Marker({
+    map: MapSettings.map,
+    content: getInfoWindowHtml(data),
+    position: new google.maps.LatLng(data.lat, data.lon),
+    title: data.title
+  });
+  MapSettings.markers[data.id] = marker;
+
+  google.maps.event.addListener(marker, 'click', function() { showInfoWindow(marker) });
+
+  return marker;
+}
+
 function getRestaurants() {
     MapSettings.path.forEach(appendArgument);
-    var url = 'https://qaus1.dev.spoon/api/v2/spoon_trips?' + MapSettings.argument_string;
+    var url = 'http://qaus1.dev.spoon/api/v2/spoon_trips?' + MapSettings.argument_string;
     jQuery.ajax({url: url}).done(function (data) {
         var response = data;
         response.forEach(function (datapoint) {
-            marker = new google.maps.Marker({
-                map: MapSettings.map,
-                position: new google.maps.LatLng(datapoint.lat, datapoint.lon),
-                title: datapoint.title
-            });
-            MapSettings.markers.push(marker);
+            setMarker(datapoint);
         });
     });
 }
